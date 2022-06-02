f982d6c84afb9a0931585a431527863986609d2e
marmac6 <mariamakrinioti@hotmail.com>
2022-06-02 20:05:16 +0300

    UTF-8 - tag(2)

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ Created on Mon May  9 12:24:21 2022
 import csv
 import deepl
 
-auth_key = 'f46e5401-8578-0112-99ca-9838bd32282e:fx'
+auth_key = 'e9c08ffa-9fc9-3eb8-eb0c-eed20d006334:fx'
 translator = deepl.Translator(auth_key)
 
-
+#mia random allagh
 def count_chars(file):
-    with open(file, 'r') as f:
+    with open(file, 'r', encoding='utf-8') as f:
         csv_reader = csv.reader(f)
 
         char_counter = 0
@@ -23,14 +23,14 @@ def count_chars(file):
     return char_counter
 
 
-def _translate_to_greek(statement: str, mock=True) -> str:
+def _translate_to_greek(statement: str, mock=True) -> str:  #to mock einai orisma
     if mock:
         return statement.replace('a', '2')
     else:
         return translator.translate_text(statement, target_lang="el").text
 
 
-def _compress_statement(statement: str) -> str:
+def _compress_statement(statement: str):
     statement = statement.replace('and', '&')
     statement = statement.replace('dollars', '$')
     statement = statement.replace('percent', '%')
@@ -72,7 +72,7 @@ def translate_dataset(entries):
     
 
     try:
-        with open(output_filename, 'a', newline='') as f:
+        with open(output_filename, 'a', newline='', encoding='utf-8') as f:
             mywriter = csv.writer(f)
             if not current_index:
                 mywriter.writerow(['Statement', 'Label'])  # add header
@@ -80,14 +80,14 @@ def translate_dataset(entries):
             for ind, entry in enumerate(entries):
                 if ind < current_index - 1:
                     continue
-                print(f'{ind} / {volume}')
+                print(f'{ind} / {volume}')   #gia na einai akribias den thelei ena +1?
                 translated = translate_statement(entry[0])
                 mywriter.writerow([translated, entry[1]])
     except FileNotFoundError as e:
         print(f'Translation aborded. Error: {e}')
 
 
-file_name = 'valid.csv'
+file_name = 'train.csv'
 output_folder = 'translated_with_api'
 csv_entries = read_csv(file_name)
 translate_dataset(csv_entries)
