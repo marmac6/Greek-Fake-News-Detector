4dbadd163ed2e2f545ae2ea9f3c9bd0df7753e31
xanthoko <mariamakrinioti@hotmail.com>
2023-01-29 23:06:48 +0200

    Refactor scrapping

--- Scraping/scraperhoaxes.py
+++ Scraping/scraperhoaxes.py
@@ -1,15 +1,21 @@
-from bs4 import BeautifulSoup
-import requests
+from  __future__ import annotations
+
 import csv
+import requests
+from bs4 import BeautifulSoup
 
 
-def getURLS():
-    url_list = []
+OUTPUT_PATH = 'myScam.csv'
+
+
+def getURLS(category: str = 'scam') -> list[str]:
+    """Retrieves all the urls in the greek hoaxes site and the different
+    categories."""
+    url_list: list[str] = []
     num_pages = 11
     for i in range(1, num_pages + 1):
         print("page: " + str(i))
-        page_url = "https://www.ellinikahoaxes.gr/category/kathgories/scam/page/" + str(
-            i)
+        page_url = f"https://www.ellinikahoaxes.gr/category/kathgories/{category}/page/{i}"
         result_pages = requests.get(page_url)
         html_pages = BeautifulSoup(result_pages.text, "html.parser")
         urls = html_pages.find_all("div", {"class": "blog-post-content"})
@@ -19,38 +25,34 @@ def getURLS():
     return url_list
 
 
-def getData(html_article):
+def get_claim_text(html_article: str) -> str:
+    """Retrieves the text inside the claim section of the given url."""
     result_article = requests.get(html_article)
     page = BeautifulSoup(result_article.text, "html.parser")
     claim = page.find("div", {"class": "Claim"})
     if claim is not None:
-        return claim.p.get_text()
-    else:
-        return ""
+        return claim.p.get_text().strip()
 
 
-def write_csv(list_fake):
-
+def write_csv(fake_news_entries: list[str]) -> None:
     try:
-        with open(file_name, 'w', newline='', encoding='utf-8') as f:
+        with open(OUTPUT_PATH, 'w', newline='', encoding='utf-8') as f:
             my_writer = csv.writer(f)
             my_writer.writerow(['title', 'is_fake'])  #add header
 
-            for ind in list_fake:
-                if ind is not None:
-                    my_writer.writerow([ind, 1])
-                else:
-                    continue
+            for fake_news_entry in fake_news_entries:
+                my_writer.writerow([fake_news_entry, 1])
     except FileNotFoundError as e:
-        print(f'Translation aborded. Error: {e}')
+        print(f'Scrapping aborted. Error: {e}')
 
 
-file_name = 'myScam.csv'
+if __name__ == '__main__':
+    fake_url_list = getURLS()
 
-fake_url_list = getURLS()
-fake_news_list = []
-for u in fake_url_list:
-    fake_news_list.append(getData(u))
-fake_news_list_no_null = list(filter(
-    None, fake_news_list))  #filter out the null values in the list
-write_csv(fake_news_list_no_null)
+    fake_news_list = []
+    for url in fake_url_list:
+        claim_text = get_claim_text(url)
+        if claim_text is not None:
+            fake_news_list.append(claim_text)
+
+    write_csv(fake_news_list)
--- Scraping/scraperreal.py
+++ Scraping/scraperreal.py
@@ -1,41 +1,41 @@
-from bs4 import BeautifulSoup
-import requests
+from __future__ import annotations
+
 import csv
+import requests
+from bs4 import BeautifulSoup
 
 
-def getTitles():
+OUTPUT_PATH = 'myCulture.csv'
+
+
+def get_titles() -> list[str]:
     titles_list = []
-    num_pages = 100
+    num_pages = 12
     for i in range(1, num_pages + 1, 5):
         print("page: " + str(i))
-        page_url = "https://www.kathimerini.gr/culture/page" + str(i)
+        page_url = f"https://www.kathimerini.gr/culture/page/{i}"
         result_pages = requests.get(page_url)
         html_pages = BeautifulSoup(result_pages.text, "html.parser")
         titles = html_pages.find_all("div",
                                      {"class": "design_one_title_medium"})
         for u in titles:
             atags = u.find_all('a')
-            titles_list.append(atags[0].string)
+            titles_list.append(atags[0].string.strip())
     return titles_list
 
 
-def write_csv(list_real):
-
+def write_csv(real_news_entries: list[str]) -> None:
     try:
-        with open(file_name, 'w', newline='', encoding='utf-8') as f:
+        with open(OUTPUT_PATH, 'w', newline='', encoding='utf-8') as f:
             my_writer = csv.writer(f)
             my_writer.writerow(['title', 'is_fake'])  #add header
 
-            for ind in list_real:
-                if ind is not None:
-                    my_writer.writerow([ind, 0])
-                else:
-                    continue
+            for real_news_entry in real_news_entries:
+                if real_news_entry is not None:
+                    my_writer.writerow([real_news_entry, 0])
     except FileNotFoundError as e:
-        print(f'Translation aborded. Error: {e}')
+        print(f'Scrapping aborded. Error: {e}')
 
 
-file_name = 'myCulture.csv'
-
-real_titles = getTitles()
+real_titles = get_titles()
 write_csv(real_titles)
